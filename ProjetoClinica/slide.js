0a123e62f4cb48bdb781e598745fc03050dfe09c
Vitor Emmanuel <97411768+BitorHasBitor@users.noreply.github.com>
2023-06-08 18:04:08 -0300

    Update slide.js

--- ProjetoClinica/slide.js
+++ ProjetoClinica/slide.js
@@ -1,1 +1,17 @@
 
+var slide = document.querySelector('.manual-btn') /*guarda a navegaçãp manual dentro de uma variável*/ 
+var cont = 1
+
+document.getElementById('slide1').checked = true
+setInterval(() => {
+    proximoSlide()
+}, 5000)
+
+function proximoSlide(){
+    cont++
+    if (cont > 3){
+        cont = 1
+    }
+
+document.getElementById('slide'+cont).checked = true
+}
